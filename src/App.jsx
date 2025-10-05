8cbe1f3d456217d9f15f09de8ecfd4b7c1bb691b
Al Muksid <deveelopment.almuksid@gmail.com>
2025-10-06 02:34:02 +0600

    Add an interactive Counter with React useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
 import BioData from './components/BioData'
-
+import { useState } from 'react'
 function App() {
 
+  const [counter, setCounter] = useState(0)
+
+  const increaseHandler = () =>{
+    setCounter(counter + 1)
+  }
+  const decreaseHandler = () =>{
+    setCounter(counter-1)
+  }
 
   return (
     <>
+      <h2>Interactive Counter with React useState Hook</h2>
+      <p>The value of the counter is {counter}</p>
+      <button onClick={increaseHandler}>Increase</button>
+      <button onClick={decreaseHandler}>decrease</button>
+
+      <hr />
       <BioData name="Al Muksid" email="web.almuksid@gmail.com" mobile="+8801770058637" linkedin="linkedin.com/in/almuksid" skills={["React.js", "Django"]} interests={["Cricket", "Football", "Badminton"]} />
       <hr />
       <BioData name="Mesion Sheikh" email="sheikh@gmail.com" mobile="+880" linkedin="linkedin.com/in/" skills={["React.js", "Django"]} interests={["Cricket", "Football", "Badminton"]} />
