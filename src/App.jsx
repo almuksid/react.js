5dfec9f6d0cf9c1c81349f09f2e4c2ec7a7f8485
Al Muksid <deveelopment.almuksid@gmail.com>
2025-10-06 13:40:16 +0600

    Class 2: Interactive Counter with React useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import './App.css'
 import BioData from './components/BioData'
-import { useState } from 'react'
+import UseStateHooks from './components/useStateHooks'
 function App() {
-
-  const [counter, setCounter] = useState(0)
-
-  const increaseHandler = () =>{
-    setCounter(counter + 1)
-  }
-  const decreaseHandler = () =>{
-    setCounter(counter-1)
-  }
-
+  
   return (
     <>
-      <h2>Interactive Counter with React useState Hook</h2>
-      <p>The value of the counter is {counter}</p>
-      <button onClick={increaseHandler}>Increase</button>
-      <button onClick={decreaseHandler}>decrease</button>
-
+      <hr />
+      <UseStateHooks/>
       <hr />
       <BioData name="Al Muksid" email="web.almuksid@gmail.com" mobile="+8801770058637" linkedin="linkedin.com/in/almuksid" skills={["React.js", "Django"]} interests={["Cricket", "Football", "Badminton"]} />
       <hr />
---
+++ src/components/UseStateHooks.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState } from 'react'
+const UseStateHooks = () => {
+    const [counter, setCounter] = useState(0)
+    
+    const increaseHandler = () =>{
+    setCounter(counter + 1)
+    }
+    const decreaseHandler = () =>{
+    setCounter(counter-1)
+    }
+  return (
+    <div>
+
+      <h2>Interactive Counter with React useState Hook</h2>
+      <p>The value of the counter is {counter}</p>
+      <button onClick={increaseHandler}>Increase</button>
+      <button onClick={decreaseHandler}>decrease</button>
+
+    </div>
+  )
+}
+
+export default UseStateHooks
